1f5355a098665927687c0f6f7218cef0addfe4b5
iamasianman <kevinle1991@gmail.com>
2023-12-14 17:37:00 -0800

    day 2 solved

---
+++ day-2/index.js
@@ -0,0 +1,77 @@
+import fs from 'fs'
+
+const input = fs.readFileSync('./input.txt').toString();
+
+
+function partOne() {
+  let total = 0
+  for (let game of input.split('\n')) {
+    let [gameId, gameData] = game.split(': ')
+    let valid = true
+
+    for (let gameSet of gameData.split('; ')) {
+      let cubeCounts = {
+        red: 12,
+        green: 13,
+        blue: 14
+      }
+
+      for (let cube of gameSet.split(', ')) {
+        let [count, color] = cube.split(' ')
+        cubeCounts[color] -= parseInt(count)
+
+        for (let key of Object.keys(cubeCounts)) {
+          if (cubeCounts[key] < 0) {
+            valid = false
+            break
+          }
+        }
+      }
+    }
+    if (valid) {
+      total += parseInt(gameId.split(' ')[1])
+    }
+  }
+  return total
+}
+
+// console.log(partOne())
+
+function partTwo() {
+  let total = 0
+  for (let game of input.split('\n')) {
+    let [gameId, gameData] = game.split(': ')
+
+    let globalCounts = {
+      red: 0,
+      green: 0,
+      blue: 0
+    }
+    for (let gameSet of gameData.split('; ')) {
+
+      let localCounts = {
+        red: 0,
+        green: 0,
+        blue: 0
+      }
+
+      for (let cube of gameSet.split(', ')) {
+        let [count, color] = cube.split(' ')
+        // localCounts[color] += parseInt(count)
+        // console.log(cubeCounts[color], "helloooooooooo")
+
+        for (let key of Object.keys(localCounts)) {
+          if (localCounts[key] > globalCounts[key]) {
+            globalCounts[key] = localCounts[key]
+          }
+        }
+      }
+    }
+    total += globalCounts.red * globalCounts.green * globalCounts.blue
+  }
+  return total
+
+}
+
+
+console.log(partTwo())
