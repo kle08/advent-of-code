d64fda1b43ff4378a217ab06517d4cdbe334fe16
iamasianman <kevinle1991@gmail.com>
2023-12-14 17:41:11 -0800

    clean up

--- day-2/index.js
+++ day-2/index.js
@@ -3,39 +3,38 @@ import fs from 'fs'
 const input = fs.readFileSync('./input.txt').toString();
 
 
-function partOne() {
-  let total = 0
-  for (let game of input.split('\n')) {
-    let [gameId, gameData] = game.split(': ')
-    let valid = true
+// function partOne() {
+//   let total = 0
+//   for (let game of input.split('\n')) {
+//     let [gameId, gameData] = game.split(': ')
+//     let valid = true
 
-    for (let gameSet of gameData.split('; ')) {
-      let cubeCounts = {
-        red: 12,
-        green: 13,
-        blue: 14
-      }
+//     for (let gameSet of gameData.split('; ')) {
+//       let cubeCounts = {
+//         red: 12,
+//         green: 13,
+//         blue: 14
+//       }
 
-      for (let cube of gameSet.split(', ')) {
-        let [count, color] = cube.split(' ')
-        cubeCounts[color] -= parseInt(count)
+//       for (let cube of gameSet.split(', ')) {
+//         let [count, color] = cube.split(' ')
+//         cubeCounts[color] -= parseInt(count)
 
-        for (let key of Object.keys(cubeCounts)) {
-          if (cubeCounts[key] < 0) {
-            valid = false
-            break
-          }
-        }
-      }
-    }
-    if (valid) {
-      total += parseInt(gameId.split(' ')[1])
-    }
-  }
-  return total
-}
+//         for (let key of Object.keys(cubeCounts)) {
+//           if (cubeCounts[key] < 0) {
+//             valid = false
+//             break
+//           }
+//         }
+//       }
+//     }
+//     if (valid) {
+//       total += parseInt(gameId.split(' ')[1])
+//     }
+//   }
+//   return total
+// }
 
-// console.log(partOne())
 
 function partTwo() {
   let total = 0
@@ -57,8 +56,7 @@ function partTwo() {
 
       for (let cube of gameSet.split(', ')) {
         let [count, color] = cube.split(' ')
-        // localCounts[color] += parseInt(count)
-        // console.log(cubeCounts[color], "helloooooooooo")
+        localCounts[color] += parseInt(count)
 
         for (let key of Object.keys(localCounts)) {
           if (localCounts[key] > globalCounts[key]) {
@@ -72,6 +70,5 @@ function partTwo() {
   return total
 
 }
-
 
 console.log(partTwo())
